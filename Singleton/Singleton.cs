4c5f46d0f14677f19d70c9d572711f951015ca51
Sonlanggtu <sonlanggtu@gmail.com>
2020-02-10 12:00:27 +0700

    Singleton Pattern

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        public static Singleton instance = new Singleton();
+        private Singleton()
+        {
+            
+        }
+
+        public string GetValue(string value)
+        {
+            return "value return" + value;
+        }
+    }
+}
